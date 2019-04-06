159c0e8f80d384655d282f5fb4146d50d336960a
diagency <44693283+diagency@users.noreply.github.com>
2019-04-07 00:48:53 +0300

    Update engineForTeachers.cpp

--- line_game/engineForTeachers/engineForTeachers.cpp
+++ line_game/engineForTeachers/engineForTeachers.cpp
@@ -16,21 +16,87 @@ string (*gamer1Step)(int ) = NULL, (*gamer2Step)(int ) = NULL;
 ofstream fout;
 
 
-string computer1(int tour_number){
+bool stepIsValid(int row, int column){
+    if (row == 0 && column == 0)
+        return false;
+    if (row < 0 || column < 0 || row >= n || column >= m)
+        return false;
+    if (row > 0 && Area[row - 1][column] == 1)
+        return false;
+    if (row < n - 1 && Area[row + 1][column] == 1)
+        return false;
+    if (column > 0 && Area[row][column - 1] == 1)
+        return false;
+    if (column < m - 1 && Area[row][column + 1] == 1)
+        return false;
+    return true;
+}
+
+string unlucky_player(int tour_number){
+    vector<string> possibleSteps;
+    if (stepIsValid(x - 1, y))
+        possibleSteps.push_back("north");
+    if (stepIsValid(x + 1, y))
+        possibleSteps.push_back("south");
+    if (stepIsValid(x, y - 1))
+        possibleSteps.push_back("west");
+    if (stepIsValid(x, y + 1))
+        possibleSteps.push_back("east");
+    if (possibleSteps.size() > 0){
+        return possibleSteps[rand() % possibleSteps.size()];
+    } 
+    return "north";
+}
+
+string tour1step(){
+    if (x < y)
+        return "south";
     return "east";
 }
 
+string tour2step(){
+    if (step == 0){
+        if (n < m)
+            return "east";
+        return "south";
+    } 
+    return tour1step();
+}
+
+string tour3step(){
+    if (stepIsValid(x - 1, y) && x % 2 == 1 && y % 2 == 0)
+        return "north";
+    if (stepIsValid(x + 1, y) && x % 2 == 1 && y % 2 == 0)
+        return "south";
+    if (stepIsValid(x, y - 1) && x % 2 == 0 && y % 2 == 1)
+        return "west";
+    if (stepIsValid(x, y + 1) && x % 2 == 0 && y % 2 == 1)
+        return "east";
+    return "north";
+}
+
+string computer1(int tour_number){
+    if (tour == 1 || tour == 3)
+        return unlucky_player(tour_number);
+    return tour2step();
+}
+
 string computer2(int tour_number){
-    return "south";
+    if (tour == 1)
+        return tour1step();
+    if (tour == 2)
+        return unlucky_player(tour_number);
+    if (tour == 3)
+        return tour3step();
 }
 
 void applySettings(){
-    gamer1Name = "Computer_1";
-    gamer2Name = "Computer_2";
+    gamer1Name = "Clever_computer_1";
+    gamer2Name = "Clever_computer_2";
     gamer1Step = &computer1;
     gamer2Step = &computer2;
-    tour = 1;
-    order = 1;
+    tour = 3;
+    order = 5;
 }
 
 void generateTour(int tour_number){
@@ -45,29 +111,15 @@ void generateTour(int tour_number){
         m = n + 1;
     }
     if (tour_number == 3){
-        n = 2 * rand() % (MAXIMAL_AREA_SIZE / 2) + 1;
-        m = 2 * rand() % (MAXIMAL_AREA_SIZE / 2) + 1;
+        n = 2 * (rand() % (MAXIMAL_AREA_SIZE / 2 - 2) + 2) + 1;
+        m = 2 * (rand() % (MAXIMAL_AREA_SIZE / 2 - 2) + 2) + 1;
     }
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
+    for (int i = 0; i < MAXIMAL_AREA_SIZE; i++){
+        for (int j = 0; j < MAXIMAL_AREA_SIZE; j++){
             Area[i][j] = 0;
         }
     }
     Area[x][y] = 2;
-}
-
-bool stepIsValid(int row, int column){
-    if (row < 0 || column < 0 || row >= n || column >= m)
-        return false;
-    if (row > 0 && Area[row - 1][column] == 1)
-        return false;
-    if (row < n - 1 && Area[row + 1][column] == 1)
-        return false;
-    if (column > 0 && Area[row][column - 1] == 1)
-        return false;
-    if (column < m - 1 && Area[row][column + 1] == 1)
-        return false;
-    return true;
 }
 
 string stepResult(string direction){
